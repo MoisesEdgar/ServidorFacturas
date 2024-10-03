cc79114716a53356ee0658c82235d899367d4f3b
MoisesEdgar <moises@macropro.com.mx>
2024-10-03 12:52:14 -0700

    Metodo PUT

--- src/main/java/com/ServidorFacturas/factura/FacturaService.java
+++ src/main/java/com/ServidorFacturas/factura/FacturaService.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class FacturaService {
@@ -117,109 +115,60 @@ public class FacturaService {
         return Integer.parseInt(salto[1]);
     }
 
-    public void calcularTotales(Long id){
-        Factura factura = repoFactura.findById(id).orElseThrow(()-> new RuntimeException("Factura no encontrada"));
-        Double subtotal = 0.0;
-        Double total = 0.0;
-        Integer cantidad = 0;
-        Double precio = 0.0;
+    public void calcularTotales(Long id) {
+        Factura factura = repoFactura.findById(id).orElseThrow(() -> new RuntimeException("Factura no encontrada"));
+        double subtotal = 0.0;
+        double total = 0.0;
 
         if (Objects.nonNull(factura.getPartidas()) && !factura.getPartidas().isEmpty()) {
             for (Partida partida : factura.getPartidas()) {
+                int cantidad = partida.getCantidad();
+                double precio = partida.getPrecio();
 
-                cantidad = partida.getCantidad();
-                precio = partida.getPrecio();
-
-                subtotal += cantidad * precio;
+                if (cantidad > 0 && precio >= 0.1) {
+                    subtotal += cantidad * precio;
+                } else {
+                    throw new RuntimeException("Error en los valores de la partida. Cantidad o precio inválido.");
+                }
             }
-            total = subtotal + subtotal * 0.16;
+            total = subtotal + (subtotal * 0.16);
 
-            factura.setSubtotal(Math.round(subtotal * 100)/100d);
-            factura.setTotal(Math.round(total * 100)/100d);
-
+            factura.setSubtotal(Math.round(subtotal * 100) / 100.0);
+            factura.setTotal(Math.round(total * 100) / 100.0);
         }
         repoFactura.save(factura);
     }
 
-
-
-
-
-
-
     public Factura updateFactura(Factura factura, Long id) {
         Factura depDB = repoFactura.findById(id).orElseThrow(() -> new RuntimeException("Factura no encontrada"));
-        List<Partida> partidasActuales = depDB.getPartidas();
-        List<Partida> partidasActualizadas = new ArrayList<>();
 
         for (Partida partida : factura.getPartidas()) {
-            if (partida.getId() != null) {
-
-                Partida partidaExistente = partidasActuales.stream()
-                        .filter(partidaActual -> partidaActual.getId().equals(partida.getId()))
-                        .findFirst()
-                        .orElse(null);
-
-
-                if (partidaExistente != null) {
 
                     if (Objects.nonNull(partida.getNombreArticulo())) {
-                        partidaExistente.setNombreArticulo(partida.getNombreArticulo());
+                        partida.setNombreArticulo(partida.getNombreArticulo());
                     } else {
                         throw new RuntimeException("No se especifico un nombre");
                     }
 
                     if (Objects.nonNull(partida.getCantidad()) && partida.getCantidad() > 0) {
-                        partidaExistente.setCantidad(partida.getCantidad());
+                      partida.setCantidad(partida.getCantidad());
                     } else {
                         throw new RuntimeException("La cantidad debe ser mayor a 0");
                     }
 
                     if (Objects.nonNull(partida.getPrecio()) && partida.getPrecio() >= 0.1) {
-                        partidaExistente.setPrecio(partida.getPrecio());
+                        partida.setPrecio(partida.getPrecio());
                     } else {
                         throw new RuntimeException("El precio debe ser mayor o igual a 0.1");
                     }
 
-                    partidasActualizadas.add(partidaExistente);
-                }
-            } else {
-                partida.setFactura(depDB);
-                partidasActualizadas.add(partida);
-
-            }
+            partida.setFactura(depDB);
+            depDB.getPartidas().add(partida);
         }
 
-
-//        for (Partida partida : factura.getPartidas()) {
-//            Partida partidaExistente = partidasActuales.stream()
-//                    .filter(partidaActual-> partidaActual.getId().equals(partida.getId()))
-//                    .findFirst()
-//                    .orElse(null);
-//
-//            if (partidaExistente == null) {
-//                repoPartida.deleteById(partida.getId());
-//            }
-//        }
-
-        depDB.setPartidas(partidasActualizadas);
         return repoFactura.save(depDB);
     }
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
 
 
 
